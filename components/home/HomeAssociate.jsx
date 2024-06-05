38d5ce28c151b47f8bd4cf55d89d15bf6ab55b14
shakibul22 <shakibulislam684@gmail.com>
2024-06-06 00:30:56 +0600

    Hover effect design

--- components/home/HomeAssociate.jsx
+++ components/home/HomeAssociate.jsx
@@ -14,26 +14,86 @@ const HomeAssociate = async () => {
   const memberList = await data;
   return (
     <div className="bg-[url('/img/memberbg.webp')] bg-cover bg-center px-2 xl:px-0 py-8 lg:py-20 text-white">
-      <div className="mx-auto max-w-screen-2xl">
+      <div className="mx-auto max-w-7xl">
         <h3 className="text-xl font-bold lg:text-4xl">
           Some of Our Associate Members
         </h3>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
-          {memberList?.slice(0, 6).map((associate, i) => (
-            <MemberListCard
-              bg={"glass"}
+          {memberList?.slice(0, 1).map((associate, i) => (
+            <div
               key={i}
-              name={associate.name}
-              member_id={associate.membershipId}
-            />
+              className="border border-white shadow-md rounded-tl-3xl p-5 rounded-bl-3xl hover:bg-sky-400/30 rounded-br-3xl rounded-tr-none hover:rounded-tr-3xl hover:rounded-br-none glass_bg "
+            >
+              <MemberListCard
+                bg={"glass"}
+                name={associate.name}
+                member_id={associate.membershipId}
+              />
+            </div>
+          ))}
+          {memberList?.slice(1, 2).map((associate, i) => (
+            <div
+              key={i}
+              className="border border-white shadow-md rounded-3xl p-5  hover:rounded-tr-none hover:rounded-bl-none hover:bg-sky-400/30 glass_bg "
+            >
+              <MemberListCard
+                name={associate.name}
+                member_id={associate.membershipId}
+              />
+            </div>
+          ))}
+          {memberList?.slice(2, 3).map((associate, i) => (
+            <div
+              key={i}
+              className="border border-white shadow-md rounded-tl-none p-5 hover:bg-sky-400/30 rounded-bl-3xl rounded-br-3xl rounded-tr-3xl hover:rounded-bl-none hover:rounded-tl-3xl glass_bg "
+            >
+              <MemberListCard
+                name={associate.name}
+                member_id={associate.membershipId}
+              />
+            </div>
+          ))}
+          {memberList?.slice(3, 4).map((associate, i) => (
+            <div
+              key={i}
+              className="border border-white shadow-md rounded-tl-3xl p-5 rounded-bl-3xl hover:bg-sky-400/30 rounded-br-none rounded-tr-3xl hover:rounded-tr-none hover:rounded-br-3xl glass_bg "
+            >
+              <MemberListCard
+                bg={"glass"}
+                name={associate.name}
+                member_id={associate.membershipId}
+              />
+            </div>
+          ))}
+          {memberList?.slice(4, 5).map((associate, i) => (
+            <div
+              key={i}
+              className="border border-white shadow-md rounded-3xl p-5  hover:rounded-tr-none hover:rounded-bl-none hover:bg-sky-400/30 glass_bg "
+            >
+              <MemberListCard
+                name={associate.name}
+                member_id={associate.membershipId}
+              />
+            </div>
+          ))}
+          {memberList?.slice(5, 6).map((associate, i) => (
+            <div
+              key={i}
+              className="border border-white shadow-md rounded-tl-3xl p-5 hover:bg-sky-400/30 rounded-bl-none rounded-br-3xl rounded-tr-3xl hover:rounded-bl-3xl hover:rounded-tl-none glass_bg "
+            >
+              <MemberListCard
+                name={associate.name}
+                member_id={associate.membershipId}
+              />
+            </div>
           ))}
         </div>
 
         <div>
           <Link
             href={"/member/list"}
-            className="py-2.5 px-5 w-fit leading-normal bg-secondaryColor block shadow-lg mx-auto mt-12 text-white font-semibold"
+            className="py-2.5 px-5 w-fit leading-normal bg-btn block shadow-lg mx-auto mt-12 text-white font-semibold"
           >
             See More
           </Link>
