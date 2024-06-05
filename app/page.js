35ec90a2ea1b3f7215de380fb7e555a349bad793
shakibul22 <shakibulislam684@gmail.com>
2024-06-06 00:28:08 +0600

    unused code removed

--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
-import Image from "next/image";
-import { Inter, Poppins } from "next/font/google";
 import HomeLayout from "@/components/home/HomeLayout";
 
-const inter = Inter({ subsets: ["latin"] });
-const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
   return (
