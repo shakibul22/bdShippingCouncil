2afbc935cfc14e31218657d4a9d2ada1625ebfa2
MD SHAKIBUL ISLAM <121640268+shakibul22@users.noreply.github.com>
2024-06-09 17:27:17 +0600

    Update FooterSub.jsx

--- components/footer/FooterSub.jsx
+++ components/footer/FooterSub.jsx
@@ -57,7 +57,7 @@ const FooterSub = () => {
           </p>
         </div>
         <form
-          className="bg-[#FFF0F0] text-[#1b405d] w-[300px] lg:w-full flex flex-row justify-between px-5 p-2 rounded-lg bg-opacity-25  text-sm"
+          className="bg-[#FFF0F0] text-[#1b405d] w-[310px] lg:w-full flex flex-row justify-between px-5 p-2 rounded-lg bg-opacity-25  text-sm"
           onSubmit={handleSub}
         >
           <input
@@ -71,7 +71,7 @@ const FooterSub = () => {
           />
           <button
             type="submit"
-            className=" bg-secondaryColor w-10 h-10 p-2 transform -translate-x-6 rounded-full"
+            className=" bg-secondaryColor w-10 h-10 p-2 transform -translate-x-12 rounded-full"
             // onClick={handleSub}
             disabled={sending}
           >
