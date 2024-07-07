74928072d8219e3e30fe795cc58f807149acb5f9
shakibul22 <shakibulislam684@gmail.com>
2024-07-07 14:54:19 +0600

    update

--- components/custom/NewsCard.jsx
+++ components/custom/NewsCard.jsx
@@ -19,7 +19,7 @@ const NewsCard = ({ image, name, date, volume, index }) => {
         className={`flex flex-row justify-end items-end absolute rounded-tl-2xl w-full bottom-0 right-0 ${
           isLargeCard
             ? "w-[130px] h-[200px] lg:w-[400px] lg:h-[330px]"
-            : "w-[70px] lg:w-[300px] h-[80px] lg:h-[200px]"
+            : "w-[70px] lg:w-[290px] h-[80px] lg:h-[190px] "
         }`}
       >
         <Image
@@ -37,7 +37,7 @@ const NewsCard = ({ image, name, date, volume, index }) => {
       <div className="flex flex-row gap-2 p-2 lg:p-4">
         <div className="font-semibold text-sm lg:text-xl text-center w-8 lg:w-10 h-8 lg:h-10"></div>
         <div className="text-xs lg:text-lg font-light text-start space-y-1 lg:space-y-2">
-          <h4 className="font-semibold text-[#0a4a77] text-xs mb-2 lg:text-2xl">
+          <h4 className="font-semibold text-[#0a4a77] text-xs mb-2 lg:text-lg 3xl:text-2xl">
             0{index}. {name}
           </h4>
           <p className="font-semibold text-[#626262] text-xs lg:text-lg">
--- components/home/HomeMember.jsx
+++ components/home/HomeMember.jsx
@@ -22,7 +22,7 @@ const HomeMember = ({ data }) => {
           className="flex flex-col lg:flex-row justify-between w-full lg:max-w-5xl 3xl:max-w-7xl mx-auto items-center gap-10 lg:mt-16"
           data-aos="fade-right"
         >
-          <div className="mt-7 text-sm lg:text-base space-y-8 text-justify lg:w-[580px] 3xl:w-[650px] ">
+          <div className="mt-7 text-sm lg:text-base space-y-8 text-justify lg:w-[480px] 3xl:w-[650px] ">
             <div className="flex items-center space-x-2">
               <p className="text-xs font-semibold lg:text-lg">
                 Become a member
@@ -32,7 +32,7 @@ const HomeMember = ({ data }) => {
               </div>
             </div>
 
-            <h2 className="text-lg font-bold lg:text-3xl mt-4 ">
+            <h2 className="text-lg font-bold  lg:text-2xl 3xl:text-3xl mt-4 ">
               Members in the{" "}
               <span className="text-btn italic font-bold tracking-wide break-words outline-text ">
                 Shippers'
