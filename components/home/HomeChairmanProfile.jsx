e78111b9cd881ff5931a764e59b7115d9e12320d
shakibul22 <shakibulislam684@gmail.com>
2024-07-07 16:01:57 +0600

    update

--- components/home/HomeChairmanProfile.jsx
+++ components/home/HomeChairmanProfile.jsx
@@ -5,10 +5,10 @@ import { ImArrowUpRight2 } from "react-icons/im";
 const HomeChairmanProfile = ({ data }) => {
 
   return (
-    <div className="bg-[url('/img/memberbg.png')] bg-cover px-4 xl:px-0 py-8 lg:py-24 text-white size-full inset-0">
+    <div className="bg-[url('/img/memberbg.png')] bg-cover px-4 xl:px-0 py-8 lg:py-20 3xl:py-24 text-white size-full inset-0">
       <div className="flex flex-col w-full lg:max-w-5xl 3xl:max-w-7xl mx-auto lg:flex-row items-center justify-center">
         <div
-          className="w-full lg:w-[30%] max-h-[410px]"
+          className="w-full lg:w-[30%] "
           data-aos-once="false"
           data-aos="fade-right"
         >
@@ -17,21 +17,21 @@ const HomeChairmanProfile = ({ data }) => {
             width={450}
             height={400}
             alt="Chairman"
-            className="mx-auto lg:mx-0 lg:h-[410px] 3xl:h-[410px]"
+            className="mx-auto lg:mx-0 lg:h-full 3xl:h-[410px]"
           />
         </div>
         <div className="text-center min-w-[55%] rounded-xl lg:text-left flex-1 bg-gradient-to-r from-gray-900 to-gray-0 bg-opacity-70 p-8 relative">
           <p className="text-md uppercase lg:text-lg font-normal relative z-10">
             Chairman’s Profile
           </p>
-          <h3 className="font-semibold text-lg leading-8 lg:text-4xl lg:mt-2 relative z-10 space-x-1">
+          <h3 className="font-semibold text-lg leading-8 lg:text-2xl 3xl:text-4xl lg:mt-1 3xl:mt-2 relative z-10 space-x-1">
             Shippers’{" "}
             <span className="text-btn font-poppins mr-1 italic font-bold tracking-wide break-words outline-text">
               Council{" "}
             </span>{" "}
             Of Bangladesh
           </h3>
-          <p className="font-normal text-sm lg:text-lg tracking-wide opacity-70 text-justify mt-4 lg:mt-7 sm:w-[60%] lg:w-auto mx-auto relative z-10">
+          <p className="font-normal text-sm lg:text-[14px] 3xl:text-[16px] tracking-wide opacity-70 text-justify mt-4  lg:mt-4 3xl:mt-7 sm:w-[60%] lg:w-auto mx-auto relative z-10">
             {data?.text}
           </p>
           <p className="mt-7 text-xl lg:text-lg relative z-10">
@@ -40,7 +40,7 @@ const HomeChairmanProfile = ({ data }) => {
           <p className="text-md lg:text-sm relative z-10">Chairman</p>
 
           <div className="flex flex-col lg:flex-row justify-evenly lg:justify-between items-center relative z-10">
-            <div className="flex items-center gap-4 w-fit mx-auto lg:mx-0 mt-2 lg:mt-4 text-sm lg:text-2xl">
+            <div className="flex items-center gap-4 w-fit mx-auto lg:mx-0 mt-2 lg:mt-2 3xl:mt-4 text-sm lg:text-2xl">
               <div className="flex gap-2 items-center">
               <div className="relative w-[30px] h-[30px] lg:w-[38px] lg:h-[38px] border bg-white hover:bg-blue-500 cursor-pointer hover:border-white rounded-full overflow-hidden flex items-center justify-center transition duration-500">
   <Link href="mailto:sn_jute@hotmail.com" className="flex items-center justify-center w-full h-full">
