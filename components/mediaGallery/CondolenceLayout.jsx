8af894c8ac46c6f0ddc7beee73623fa4fdf6b4de
shakibul22 <shakibulislam684@gmail.com>
2024-07-28 02:24:34 +0600

    condolence text removed

--- components/mediaGallery/CondolenceLayout.jsx
+++ components/mediaGallery/CondolenceLayout.jsx
@@ -14,7 +14,7 @@ const CondolenceLayout = async () => {
       "featured": true,
       "image": "",
       "link":"",
-      "text": "“Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum“"
+      "text": " "
     },
     {
       "id": 2,
